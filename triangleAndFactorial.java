146adc1299394d38256723bbba0d3a787e83f7a8
alexandr polotovsky <apolotovsky@gmail.com>
2022-11-23 16:30:13 +0300

    add calculation n-triangle

--- triangleAndFactorial.java
+++ triangleAndFactorial.java
@@ -3,23 +3,34 @@ import java.util.Scanner;
 /* Вычислить n-ое треугольного число(сумма чисел от 1 до n), 
 n! (произведение чисел от 1 до n) */
 
-
 public class triangleAndFactorial {
 
-    static int getFactorialR(int n){
+    static int getFactorialR(int n) {
         if (n <= 1) {
             return 1;
         } else {
-            return getFactorialR(n -1) * n;
+            return getFactorialR(n - 1) * n;
         }
     }
 
-
-    static int getFactorial(int n){
+    static int getFactorial(int n) {
         int result = 1;
         for (int i = 1; i <= n; i++) {
             result *= i;
         }
+        return result;
+    }
+
+    static int getTriangleR(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return getTriangleR(n - 1) + n;
+        }
+    }
+
+    static int getTriangle(int n) {
+        int result = (n * (n + 1)) / 2;
         return result;
     }
 
@@ -30,8 +41,11 @@ public class triangleAndFactorial {
 
         int res1 = getFactorial(n);
         int res2 = getFactorialR(n);
-    
+        int res3 = getTriangleR(n);
+        int res4 = getTriangle(n);
+
         System.out.println("n! = " + res1 + ", n! рекурсивно = " + res2);
+        System.out.printf("n-triangle рекурсивно =  %d , n-triangle math = %d\n", res3, res4);
     }
-    
+
 }
